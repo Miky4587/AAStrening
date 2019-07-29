3136c2fd62dcf1b1984e66f14c40ea2687b1bb59
Miky4587 <mmikos@pobox.sk>
2019-07-29 14:52:39 +0200

    Initial - add test on the checking result calculation v2

--- src/test/java/Test/CalculationTest.java
+++ src/test/java/Test/CalculationTest.java
@@ -23,6 +23,7 @@ public class CalculationTest {
     @Test
     public void itShouldCalculateTotalIncome() {
         new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText("McDuck's safe");
+        selectfund("McDuck's safe");
         driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys("5000");
         driver.findElement(By.id("yearsInput")).sendKeys("10");
         driver.findElement(By.id("emailInput")).sendKeys("aaa@bbb.ccc");
@@ -41,12 +42,46 @@ public class CalculationTest {
 
     @Test
     public void itShouldCalculateRisk() {
+        selectfund("McDuck's safe");
+        oneTimeInvestmentInput("5000");
+        enterYears( "10");
+
         new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText("McDuck's safe");
         driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys("5000");
         driver.findElement(By.id("yearsInput")).sendKeys("10");
         driver.findElement(By.id("emailInput")).sendKeys("aaa@bbb.ccc");
     }
 
+    @Test
+    public void itShouldCalculateTotoalForEachFund(){
+        String[] arrayOfFunds = {"Batman's Cave Development", "McDuck's safe", "Tom & Jerry corp"};
+        for (String arrayOfFund : arrayOfFunds) {
+            selectfund(arrayOfFund);
+            oneTimeInvestmentInput("5000");
+            enterYears( "10");
+            Assert.assertFalse(driver
+                    .findElement(By.cssSelector("div.result > div:nth-child(1) > p"))
+                    .getText()
+                    .isEmpty());
+            Assert.assertTrue(driver
+                    .findElement(By.cssSelector("div.result > div:nth-child(1) > p"))
+                    .getText()
+                    .contains("kr"));
+        }
+    }
+
+
+    private void selectfund(String fundSelect){
+        new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText("fundSelect");
+    }
+
+    private void oneTimeInvestmentInput(String amountToEnter){
+        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys(amountToEnter);
+    }
+
+    private void enterYears(String yearsToEnter){
+        driver.findElement(By.id("yearsInput")).sendKeys(yearsToEnter);
+    }
 
     @After
     public void TearDown() {
