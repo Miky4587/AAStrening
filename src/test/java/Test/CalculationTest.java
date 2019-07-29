7069bc618e14db33fc3e7e7c01cf9f9eae645510
Miky4587 <mmikos@pobox.sk>
2019-07-29 13:16:24 +0200

    Initial - add test on the checking result calculation

---
+++ src/test/java/Test/CalculationTest.java
@@ -0,0 +1,57 @@
+package Test;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CalculationTest {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://localhost/savingscalculator.php");
+    }
+
+    @Test
+    public void itShouldCalculateTotalIncome() {
+        new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText("McDuck's safe");
+        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys("5000");
+        driver.findElement(By.id("yearsInput")).sendKeys("10");
+        driver.findElement(By.id("emailInput")).sendKeys("aaa@bbb.ccc");
+        driver.findElement(By.cssSelector("div.result > div:nth-child(1) > p")).getText();
+        Assert.assertFalse(driver.findElement(By.cssSelector("div.result > div:nth-child(1) > p")).getText().isEmpty());
+    }
+
+    @Test
+    public void itShouldCalculateInterestIncome() {
+        new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText("McDuck's safe");
+        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys("5000");
+        driver.findElement(By.id("yearsInput")).sendKeys("10");
+        driver.findElement(By.id("emailInput")).sendKeys("aaa@bbb.ccc");
+    }
+
+    @Test
+    public void itShouldCalculateRisk() {
+        new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText("McDuck's safe");
+        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys("5000");
+        driver.findElement(By.id("yearsInput")).sendKeys("10");
+        driver.findElement(By.id("emailInput")).sendKeys("aaa@bbb.ccc");
+    }
+
+
+    @After
+    public void TearDown() {
+        driver.close();
+        driver.quit();
+    }
+
+}
+
