4170612174ee5b25a9c60350c881bc2991d7da5c
Miky4587 <mmikos@pobox.sk>
2019-07-30 15:51:33 +0200

    Initial - add test on the checking result calculation v8 30/07/2019

--- src/test/java/test/EmailValidationTest.java
+++ src/test/java/test/EmailValidationTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import pages.CalculatorPage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EmailValidationTest extends TestBase {
     @Test
     public void itShouldDisplayErrorWhenEmailListIsInvalid() {
@@ -12,5 +20,13 @@ public class EmailValidationTest extends TestBase {
         calculatorPage.enterEmail("arsenal");
         System.out.println(driver.findElement(By.xpath("//div[input[@id='emailInput']]")).getAttribute("class").contains("error"));
     };
-}
 
+
+    private List<String> readInvalidEmails() throws FileNotFoundException {
+    FileReader invalidtxt = new FileReader(new File("src/test/resources/invalid.txt"));
+    List<String> invalidEmailList = new ArrayList<String>();
+    invalidEmailList = new BufferedReader(invalidtxt).lines().collect(Collectors.toList());
+    return invalidEmailList;
+
+    }
+}
